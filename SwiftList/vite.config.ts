58ee3eb400a5a5e000bf72d745ef2608c9d48376
Mihir Mehta <i.mihir.mehta@gmail.com>
2026-01-01 14:59:48 -0800

    setup tailwind css and shadcn

--- SwiftList/vite.config.ts
+++ SwiftList/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,10 +19,16 @@ export default defineConfig({
   },
 
   plugins: [
+    tailwindcss(),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  }
 })
